a85f0ab3f6c341ee0c2ffb253e922f5c7ae95dc5
Warren W. Kretzschmar <wkretzsch@gmail.com>
2017-10-14 18:09:16 +0100

    All kmer_counter tests pass

--- kmer_counter.py
+++ kmer_counter.py
@@ -1,2 +1,10 @@
-def count(record):
-    return {record: 1}
+import collections
+
+
+def count(record, kmer_size):
+    counts = collections.defaultdict(lambda: 0)
+    assert kmer_size > 0
+    assert len(record) >= kmer_size
+    for kmer_idx in range(len(record) - kmer_size + 1):
+        counts[record[kmer_idx:(kmer_idx + kmer_size)]] += 1
+    return counts
--- test_kmer_counter.py
+++ test_kmer_counter.py
@@ -1,14 +1,64 @@
+import pytest
+
 import kmer_counter
 
 
 class TestCount(object):
+    def test_with_empty_record_raises(self):
+        with pytest.raises(AssertionError):
+            kmer_counter.count('', 3)
+
+    def test_with_kmer_size_0_raises(self):
+        with pytest.raises(AssertionError):
+            kmer_counter.count('A', 0)
+
+    def test_with_record_length_2_and_kmer_length_3_raises(self):
+        with pytest.raises(AssertionError):
+            kmer_counter.count('AA', 3)
+
     def test_with_record_size_3_returns_kmer_size_3(self):
         # given
         record = 'AAA'
 
         # when
-        count = kmer_counter.count(record)
+        count = kmer_counter.count(record, 3)
 
         # then
         assert list(count.keys()) == [record]
         assert list(count.values()) == [1]
+
+    def test_with_record_size_4_returns_one_kmer_count_2(self):
+        # given
+        record = 'AAAA'
+        kmer_size = 3
+        # when
+        count = kmer_counter.count(record, kmer_size)
+
+        # then
+        assert list(count.keys()) == [record[:kmer_size]]
+        assert list(count.values()) == [2]
+
+    def test_with_two_kmers_returns_two_kmers_size_3(self):
+        # given
+        record = 'AAAT'
+        kmer_size = 3
+        # when
+        count = kmer_counter.count(record, kmer_size)
+
+        # then
+        assert count['AAA'] == 1
+        assert count['AAT'] == 1
+
+    def test_with_two_kmers_returns_two_kmers_size_1(self):
+        # given
+        record = 'AT'
+        kmer_size = 1
+        # when
+        count = kmer_counter.count(record, kmer_size)
+
+        # then
+        assert count['A'] == 1
+        assert count['T'] == 1
+
+
+
